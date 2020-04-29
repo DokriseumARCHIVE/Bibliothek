f646cf9cc5866ce8b6d72acadcb717f473a22a56
Dustin <eiki300499@gmail.com>
2020-04-29 16:37:37 +0200

    Update Tests Lastname

--- src/BibliotheksverzeichnisTest.java
+++ src/BibliotheksverzeichnisTest.java
@@ -15,6 +15,20 @@ class BibliotheksverzeichnisTest {
         testBiblio = biblioTest.getBiblio();
     }
 
+    @Test
+    void gutAddABook() {
+        //SETUP//
+        String testTitle = "Java ist auch eine Insel";
+        String testYear = "2019";
+        String testAuthor = "Christian Ullenboom";
+        Bibliotheksverzeichnis testLocalBiblio = testBiblio;
+        //TEST//
+        testLocalBiblio.addBuch(new Buch(testTitle, testAuthor, testYear));
+        Assertions.assertEquals(testAuthor, testLocalBiblio.getBuch(5).getAuthor());
+        Assertions.assertEquals(testTitle, testLocalBiblio.getBuch(5).getTitle());
+        Assertions.assertEquals(testYear, testLocalBiblio.getBuch(5).getPublicationYear());
+    }
+
     @org.junit.jupiter.api.Test
     void gutSearchByLastnameNormalfall1() {
         // SETUP//
@@ -28,33 +42,44 @@ class BibliotheksverzeichnisTest {
     @org.junit.jupiter.api.Test
     void gutSearchByLastnameNormalfall2() {
         // SETUP//
-        String testString = "";
+        String testString = "Gefundene Buecher:\n" +
+                "\tRonald Dump, Mandy Sult: Java mal ganz anders, 2010\n" +
+                "\tRonald Dump: Theoretisch gesagt, praktisch gemacht, 2015\n";
         // TEST//
-        Assertions.assertEquals(testString, testBiblio.searchByLastname("Sult"));
+        Assertions.assertEquals(testString, testBiblio.searchByLastname("Dump"));
     }
 
     @org.junit.jupiter.api.Test
     void gutSearchByLastnameNormalfall3() {
         // SETUP//
-        String testString = "";
+        String testString = "Gefundene Buecher:\n" +
+                "\tAnette Neumann: Grundlagen theoretischer Physik, 2007\n";
         // TEST//
-        Assertions.assertEquals(testString, testBiblio.searchByLastname("Sult"));
+        Assertions.assertEquals(testString, testBiblio.searchByLastname("Neumann"));
     }
 
     @org.junit.jupiter.api.Test
     void gutSearchByLastnameNormalfall4() {
         // SETUP//
-        String testString = "";
+        String testString = "Gefundene Buecher:\n" +
+                "\tThomas Ulm: Grundlagen der Programmierung mit Java, 2016\n";
         // TEST//
-        Assertions.assertEquals(testString, testBiblio.searchByLastname("Sult"));
+        Assertions.assertEquals(testString, testBiblio.searchByLastname("Ulm"));
     }
 
     @org.junit.jupiter.api.Test
     void gutSearchByLastnameNormalfall5() {
-        // SETUP//
-        String testString = "";
+        //SETUP//
+        String testTitle = "Java ist auch eine Insel";
+        String testYear = "2019";
+        String testAuthor = "Christian Ullenboom";
+        Bibliotheksverzeichnis testLocalBiblio = testBiblio;
+        testLocalBiblio.addBuch(new Buch(testTitle, testAuthor, testYear));
+        //TEST//
+        String testString = "Gefundene Buecher:\n" +
+                "\tChristian Ullenboom: Java ist auch eine Insel, 2019\n";
         // TEST//
-        Assertions.assertEquals(testString, testBiblio.searchByLastname("Sult"));
+        Assertions.assertEquals(testString, testLocalBiblio.searchByLastname("Ullenboom"));
     }
 
     @org.junit.jupiter.api.Test
@@ -66,19 +91,5 @@ class BibliotheksverzeichnisTest {
                 "\tAnette Neumann: Grundlagen theoretischer Physik, 2007\n";
         // TEST//
         Assertions.assertEquals(testString, testBiblio.searchByKeyword("theoretisch"));
-    }
-
-    @Test
-    void gutAddABook() {
-        //SETUP//
-        String testTitle = "Java ist auch eine Insel";
-        String testYear = "2019";
-        String testAuthor = "Christian Ullenboom";
-        Bibliotheksverzeichnis testLocalBiblio = testBiblio;
-        //TEST//
-        testLocalBiblio.addBuch(new Buch(testTitle, testAuthor, testYear));
-        Assertions.assertEquals(testAuthor, testLocalBiblio.getBuch(5).getAuthor());
-        Assertions.assertEquals(testTitle, testLocalBiblio.getBuch(5).getTitle());
-        Assertions.assertEquals(testYear, testLocalBiblio.getBuch(5).getPublicationYear());
     }
 }
--- src/Start.java
+++ src/Start.java
@@ -9,31 +9,10 @@ public class Start {
             System.out.println(biblio.searchByLastname("Sult"));
             System.out.println();
             System.out.println(biblio.searchByKeyword("theoretisch"));
-            String string1 = biblio.searchByLastname("Sult");
             System.out.println();
-            String string2 = biblio.searchByKeyword("theoretisch");
-            System.out.println();
+            System.out.println(biblio.searchByLastname("Ulm"));
         } catch (Exception e) {
             e.getMessage();
         }
-
-        /**
-         try {
-         FileWriter fw = new FileWriter("ausgabe.txt");
-         PrintWriter pw = new PrintWriter(fw);
-         pw.println(string1);
-         pw.println();
-         pw.println(string2);
-         pw.close();
-         } catch (FileNotFoundException e) {
-         System.err.println("Die Datei wurde nicht gefunden.");
-         } catch (FileAlreadyExistsException e) {
-         System.err.println("Datei existiert bereits.");
-         } catch (IOException e) {
-         System.err.println("Eingabe- oder Ausgabefehler!");
-         } catch (Exception e) {
-         System.err.println("Fehler!");
-         }
-         */
     }
 }
